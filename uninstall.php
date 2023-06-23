4858966bc2c09efa2f46b91437dd92ba2c26a7ee
Corey Salzano <corey.salzano@gmail.com>
2023-06-23 12:37:24 -0400

    Adds an uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Embed PDF for Gravity Forms Uninstaller
+ *
+ * @package embed-pdf-gravityforms
+ */
+
+// If uninstall.php is not called by WordPress, die.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
+
+$slug = 'embedpdfviewer';
+
+delete_option( "gravityformsaddon_{$slug}_version" );
+delete_option( "gravityformsaddon_{$slug}_settings" );
+$wpdb::delete( "{$wpdb->prefix}gf_addon_feed", array( 'addon_slug' => $slug ) );
