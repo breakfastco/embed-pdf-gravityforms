bc3596ca0382a128967edaa3acba702a62848c61
Corey Salzano <938411+csalzano@users.noreply.github.com>
2023-09-05 18:34:12 -0400

    Changes comments only

--- includes/class-gf-field-pdf-viewer.php
+++ includes/class-gf-field-pdf-viewer.php
@@ -134,7 +134,7 @@ class GF_Field_PDF_Viewer extends GF_Field {
 		// Do we even have a PDF?
 		if ( empty( $url ) ) {
 			// No.
-			// Are we on a feed settings page? This isn't a problem when configuring feeds in the pro version.
+			// Are we on a feed settings page? This isn't a problem when configuring feeds in Inkless.
 			if ( 'form_settings_embedpdfviewerpro' !== GFForms::get_page() ) {
 				$this->log_error( sprintf( __( 'No PDF to load into field %1$s on form %2$s', 'embed-pdf-gravityforms' ), $this->id, $form['id'] ) );
 				return;
