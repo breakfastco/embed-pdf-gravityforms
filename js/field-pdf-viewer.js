f4efc656fd89dff1417e3136e85a366d6bee9c6e
Corey Salzano <938411+csalzano@users.noreply.github.com>
2023-09-27 11:19:22 -0400

    Reorders methods in the field script file. Adds comments. Removes the isValidHttpUrl() method because it is used by the form-editor script only.

--- js/field-pdf-viewer.js
+++ js/field-pdf-viewer.js
@@ -1,15 +1,4 @@
 (function( epdfGf, undefined ) {
-	function isValidHttpUrl(string) {
-		let url;
-	
-		try {
-			url = new URL(string);
-		} catch (_) {
-			return false;
-		}
-	
-		return url.protocol === "http:" || url.protocol === "https:";
-	}
 
 	// Loading, paging, & zooming pdf.js viewers
 	window.addEventListener( 'load', function(e) {
@@ -34,126 +23,7 @@
 	function canvasElement( fieldId, formId ) {
 		return document.querySelector( '.epdf-container canvas.epdf#field_' + formId + '_' + fieldId + '_embed_pdf_gravityforms' );
 	}
-	
-	/**
-	 * Get page info from document, resize canvas accordingly, and render page.
-	 * @param num Page number.
-	 */
-	function renderPage( epdfInstance, pageNum ) {
-		if ( isNaN( Number(pageNum) ) ) {
-			pageNum = 1;
-		}
-		epdfInstance.dataset.pageRendering = true;
-		// Using promise to fetch the page
-		window['epdf_gf'][epdfInstance.dataset.field]['pdfDoc'].getPage(pageNum).then(function(page) {
-			var viewport = page.getViewport({scale: window['epdf_gf'][epdfInstance.dataset.field]['pdfDoc'].currentScaleValue});
-			epdfInstance.height = viewport.height;
-			epdfInstance.width = viewport.width;
-	
-			// Render PDF page into canvas context
-			var renderContext = {
-				canvasContext: epdfInstance.getContext('2d'),
-				viewport: viewport
-			};
-			var renderTask = page.render(renderContext);
-	
-			// Wait for rendering to finish
-			renderTask.promise.then(function() {
-				epdfInstance.dataset.pageRendering = false;
-				if (epdfInstance.dataset.pageNumPending !== '') {
-					// New page rendering is pending
-					renderPage(epdfInstance, Number(epdfInstance.dataset.pageNumPending));
-					epdfInstance.dataset.pageNumPending = '';
-				}
-	
-				// Set the canvas width once or else zoom in and out break
-				epdfInstance.style.width = '100%';
-				epdfInstance.style.width = epdfInstance.width + 'px';
-	
-				// Dispatch an event after a page render.
-				const event = new CustomEvent( 'epdf_gf_render_page', { detail: epdfInstance.dataset.pageNum });
-				window.dispatchEvent(event);
-			});
-		});
-	
-		// Update page counters
-		document.getElementById( epdfInstance.id + '_page_num').textContent = pageNum;
-	}
-	/**
-	 * If another page rendering in progress, waits until the rendering is
-	 * finised. Otherwise, executes rendering immediately.
-	 */
-	function queueRenderPage(epdfInstance) {
-		if ('true' === epdfInstance.dataset.pageRendering) {
-			epdfInstance.dataset.pageNumPending = epdfInstance.dataset.pageNum;
-		} else {
-			renderPage(epdfInstance, Number(epdfInstance.dataset.pageNum));
-		}
-	}
-	/**
-	 * Displays previous page.
-	 */
-	epdfGf.onPrevPage = function(e) {
-		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
-		if (Number(epdfInstance.dataset.pageNum) <= 1) {
-			return;
-		}
-		epdfInstance.dataset.pageNum--;
-		queueRenderPage(epdfInstance);
-		togglePrevNextButtons(epdfInstance);
-	}
-	/**
-	 * Displays next page.
-	 */
-	epdfGf.onNextPage = function(e) {
-		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
-		if (Number(epdfInstance.dataset.pageNum) >= window['epdf_gf'][e.target.dataset.field]['pdfDoc'].numPages) {
-			return;
-		}
-		epdfInstance.dataset.pageNum++;
-		queueRenderPage(epdfInstance);
-		togglePrevNextButtons(epdfInstance);
-	}
-	function togglePrevNextButtons( epdfInstance ) {
-		document.getElementById( epdfInstance.id + '_prev').disabled = ( 1 == epdfInstance.dataset.pageNum );
-		document.getElementById( epdfInstance.id + '_next').disabled = ( epdfInstance.dataset.pageNum == window['epdf_gf'][epdfInstance.dataset.field]['pdfDoc'].numPages );
-	}
-	function scaleDeltaDefault() {
-		return 1.1;
-	}
-	function scaleMin() {
-		return 0.25;
-	}
-	function scaleMax() {
-		return 10.0;
-	}
-	
-	epdfGf.onZoomIn = function(e) {
-		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
-		let newScale = window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue;
-		newScale = (newScale * scaleDeltaDefault()).toFixed(2);
-		newScale = Math.ceil(newScale * 10) / 10;
-		newScale = Math.min(scaleMax(), newScale);
-		window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue = newScale;
-		renderPage(epdfInstance, Number(epdfInstance.dataset.pageNum));
-	
-		// Dispatch an event about the new scale value.
-		const event = new CustomEvent( 'epdf_gf_scale_value', { detail: newScale });
-		window.dispatchEvent(event);
-	}
-	epdfGf.onZoomOut = function(e) {
-		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
-		let newScale = window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue;
-		newScale = (newScale / scaleDeltaDefault()).toFixed(2);
-		newScale = Math.floor(newScale * 10) / 10;
-		newScale = Math.max(scaleMin(), newScale);
-		window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue = newScale;
-		renderPage(epdfInstance, Number(epdfInstance.dataset.pageNum));
-	
-		// Dispatch an event about the new scale value.
-		const event = new CustomEvent( 'epdf_gf_scale_value', { detail: newScale });
-		window.dispatchEvent(event);
-	}
+
 	function loadPreview( fieldId, formId ) {
 		var epdfInstance = canvasElement( fieldId, formId );
 		var fieldElementId = 'field_' + formId + '_' + fieldId;
@@ -164,7 +34,7 @@
 			}
 			return;
 		}
-	
+
 		var urlEl = document.getElementById( 'input_' + formId + '_' + fieldId );
 		if ( null === urlEl || '' === urlEl.value ) {
 			// The URL input is missing, or there is no PDF to load.
@@ -173,7 +43,7 @@
 			}
 			return;
 		}
-	
+
 		const controls = {
 			'prev': 'onPrevPage',
 			'next': 'onNextPage',
@@ -186,7 +56,7 @@
 				el.addEventListener('click', epdfGf[controls[key]]);
 			}
 		});
-	
+
 		/**
 		 * Asynchronously downloads PDF.
 		 */
@@ -197,13 +67,13 @@
 			window['epdf_gf'][fieldId]['pdfDoc'] = pdfDoc_;
 			document.getElementById( epdfInstance.id + '_page_count').textContent = window['epdf_gf'][fieldId]['pdfDoc'].numPages;
 			window['epdf_gf'][fieldId]['pdfDoc'].currentScaleValue = epdfInstance.dataset.initialScale;
-	
+
 			// Blow up the canvas to 100% width before rendering
 			epdfInstance.style.width = '100%';
-	
+
 			// Initial/first page rendering
 			renderPage(epdfInstance, Number(epdfInstance.dataset.pageNum));
-	
+
 			// Disable the Previous or Next buttons depending on page count.
 			togglePrevNextButtons(epdfInstance);
 		}).catch(function(error){
@@ -226,4 +96,155 @@
 			const controlEls = document.querySelectorAll( '#' + fieldElementId + ' .epdf-controls-container, #' + fieldElementId + ' .epdf-container' ).forEach( function( el ) { el.style.display ='none'; });
 		});
 	}
+
+	/**
+	 * If another page rendering in progress, waits until the rendering is
+	 * finised. Otherwise, executes rendering immediately.
+	 */
+	function queueRenderPage(epdfInstance) {
+		if ('true' === epdfInstance.dataset.pageRendering) {
+			epdfInstance.dataset.pageNumPending = epdfInstance.dataset.pageNum;
+		} else {
+			renderPage(epdfInstance, Number(epdfInstance.dataset.pageNum));
+		}
+	}
+	
+	/**
+	 * Get page info from document, resize canvas accordingly, and render page.
+	 * @param num Page number.
+	 */
+	function renderPage( epdfInstance, pageNum ) {
+		if ( isNaN( Number(pageNum) ) ) {
+			pageNum = 1;
+		}
+		epdfInstance.dataset.pageRendering = true;
+		// Using promise to fetch the page
+		window['epdf_gf'][epdfInstance.dataset.field]['pdfDoc'].getPage(pageNum).then(function(page) {
+			var viewport = page.getViewport({scale: window['epdf_gf'][epdfInstance.dataset.field]['pdfDoc'].currentScaleValue});
+			epdfInstance.height = viewport.height;
+			epdfInstance.width = viewport.width;
+
+			// Render PDF page into canvas context
+			var renderContext = {
+				canvasContext: epdfInstance.getContext('2d'),
+				viewport: viewport
+			};
+			var renderTask = page.render(renderContext);
+
+			// Wait for rendering to finish
+			renderTask.promise.then(function() {
+				epdfInstance.dataset.pageRendering = false;
+				if (epdfInstance.dataset.pageNumPending !== '') {
+					// New page rendering is pending
+					renderPage(epdfInstance, Number(epdfInstance.dataset.pageNumPending));
+					epdfInstance.dataset.pageNumPending = '';
+				}
+
+				// Set the canvas width once or else zoom in and out break
+				epdfInstance.style.width = '100%';
+				epdfInstance.style.width = epdfInstance.width + 'px';
+
+				// Dispatch an event after a page render.
+				const event = new CustomEvent( 'epdf_gf_render_page', { detail: epdfInstance.dataset.pageNum });
+				window.dispatchEvent(event);
+			});
+		});
+
+		// Update page counters
+		document.getElementById( epdfInstance.id + '_page_num').textContent = pageNum;
+	}
+
+	function scaleDeltaDefault() {
+		return 1.1;
+	}
+
+	function scaleMax() {
+		return 10.0;
+	}
+
+	function scaleMin() {
+		return 0.25;
+	}
+
+	function togglePrevNextButtons( epdfInstance ) {
+		document.getElementById( epdfInstance.id + '_prev').disabled = ( 1 == epdfInstance.dataset.pageNum );
+		document.getElementById( epdfInstance.id + '_next').disabled = ( epdfInstance.dataset.pageNum == window['epdf_gf'][epdfInstance.dataset.field]['pdfDoc'].numPages );
+	}
+
+	/**
+	 * Public Methods
+	 */
+
+	/**
+	 * Handler for the Previous Page button.
+	 * 
+	 * @param {*} e 
+	 * @returns 
+	 */
+	epdfGf.onPrevPage = function(e) {
+		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
+		if (Number(epdfInstance.dataset.pageNum) <= 1) {
+			return;
+		}
+		epdfInstance.dataset.pageNum--;
+		queueRenderPage(epdfInstance);
+		togglePrevNextButtons(epdfInstance);
+	}
+
+	/**
+	 * Handler for the Next Page button.
+	 * 
+	 * @param {*} e 
+	 * @returns 
+	 */
+	epdfGf.onNextPage = function(e) {
+		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
+		if (Number(epdfInstance.dataset.pageNum) >= window['epdf_gf'][e.target.dataset.field]['pdfDoc'].numPages) {
+			return;
+		}
+		epdfInstance.dataset.pageNum++;
+		queueRenderPage(epdfInstance);
+		togglePrevNextButtons(epdfInstance);
+	}
+
+	/**
+	 * Handler for the Zoom In button.
+	 * 
+	 * @param {*} e 
+	 * @returns 
+	 */
+	epdfGf.onZoomIn = function(e) {
+		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
+		let newScale = window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue;
+		newScale = (newScale * scaleDeltaDefault()).toFixed(2);
+		newScale = Math.ceil(newScale * 10) / 10;
+		newScale = Math.min(scaleMax(), newScale);
+		window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue = newScale;
+		renderPage(epdfInstance, Number(epdfInstance.dataset.pageNum));
+	
+		// Dispatch an event about the new scale value.
+		const event = new CustomEvent( 'epdf_gf_scale_value', { detail: newScale });
+		window.dispatchEvent(event);
+	}
+
+	/**
+	 * Handler for the Zoom Out button.
+	 * 
+	 * @param {*} e 
+	 * @returns 
+	 */
+	epdfGf.onZoomOut = function(e) {
+		var epdfInstance = canvasElement( e.target.dataset.field, e.target.dataset.form );
+		let newScale = window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue;
+		newScale = (newScale / scaleDeltaDefault()).toFixed(2);
+		newScale = Math.floor(newScale * 10) / 10;
+		newScale = Math.max(scaleMin(), newScale);
+		window['epdf_gf'][e.target.dataset.field]['pdfDoc'].currentScaleValue = newScale;
+		renderPage(epdfInstance, Number(epdfInstance.dataset.pageNum));
+	
+		// Dispatch an event about the new scale value.
+		const event = new CustomEvent( 'epdf_gf_scale_value', { detail: newScale });
+		window.dispatchEvent(event);
+	}
+
 }( window.epdfGf = window.epdfGf || {} ));
