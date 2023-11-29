5b8cca92d5ddb11150d759252f5d26ae1c0e5933
Corey Salzano <938411+csalzano@users.noreply.github.com>
2023-11-29 10:21:53 -0500

    Bug fix in the uninstaller, use the global $wpdb

--- uninstall.php
+++ uninstall.php
@@ -14,4 +14,9 @@ $slug = 'embedpdfviewer';
 
 delete_option( "gravityformsaddon_{$slug}_version" );
 delete_option( "gravityformsaddon_{$slug}_settings" );
-$wpdb::delete( "{$wpdb->prefix}gf_addon_feed", array( 'addon_slug' => $slug ) );
+global $wpdb;
+$wpdb->delete(
+	"{$wpdb->prefix}gf_addon_feed",
+	array( 'addon_slug' => $slug ),
+	array( '%s' )
+);
